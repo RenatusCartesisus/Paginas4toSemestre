ef8850518e2933ce3d2d524944bab943efeac391
Givrain Ortega <ortegagivrain@gmail.com>
2023-02-10 07:07:43 -0600

    Agrego programas hechos a base de aechivos HTML y JS independientes

---
+++ Cliente/Practicas_js/Basico/JavaScript/Ejercicio5-2.js
@@ -0,0 +1,10 @@
+let edad = 15;
+let nombre = "Rene";
+let apellido = "Favaloro";
+let sueldo = 206.5;
+let sueltoT;
+
+document.getElementById('ejemplo').innerHTML = `Mi nombre es ${nombre} ${apellido}, y mi sueldo es de ${sueldoT = sueldo*6} pesos.`;
+
+let maestros = ["Daniel Mendez", "Mario Perez", "Mauro Rojas", "Luna"];
+document.write(maestros[1]);
